7e1d8eed0852c4731c978f8b4b2f000f84dc20dd
Kevin Berry <kevin@opensourcealchemist.com>
2011-04-30 18:49:55 -0500

    Added a little ruby to update the list of config files to be listed in backup on the PKGBUILD.  Makes maintenance of the PKGBUILD file easier when more services are added.

---
+++ update_backups.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require "find"
+
+configs = [] # Array to hold list of config files to save
+Find.find("etc") do |path|
+  configs << path if File.basename(path) == 'run'
+  configs << path if File.basename(path) == 'finish'
+  configs << path if File.dirname(path).match(/env/)
+  configs << path if File.dirname(path).match(/control/)
+end
+
+pkgbuild = File.readlines("PKGBUILD")
+
+File.opens("PKGBUILD","w") do |f|
+  pkgbuild.each do |l|
+    l.strip!
+    if l[0,6] == "backup"
+      l = "backup=(\'#{configs.join("\' \'")}\')"
+    end
+    f.puts l
+  end
+end
