9a18981ec51ce117a72822a0690a5efddde04c17
TJ Vanderpoel <tj@rubyists.com>
2013-03-03 23:55:06 -0600

    updated md5

--- make-release.sh
+++ make-release.sh
@@ -5,7 +5,7 @@ tmpdir=$(mktemp -d)
 cd $tmpdir
 git clone $curdir
 checkout=${curdir##*/}
-rm -rf $checkout/.git $checkout/make-release.sh $checkout/.gitignore  $checkout/PKGBUILD
+rm -rf $checkout/.git $checkout/make-release.sh $checkout/.gitignore  $checkout/PKGBUILD $checkout/update_backups.rb
 tar czvf "${pkgname}-${pkgver}-${pkgrel}.tar.gz" $checkout
 cp "${pkgname}-${pkgver}-${pkgrel}.tar.gz" $curdir
 echo "Wrote ${pkgname}-${pkgver}-${pkgrel}.tar.gz"
